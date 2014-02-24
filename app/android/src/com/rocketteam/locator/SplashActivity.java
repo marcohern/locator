a13f41ac5d16beda90517278fa000092004d3c8c
Marco Hernandez <marcohern@gmail.com>
2014-02-23 20:57:24 -0500

    Fixxed many issues

---
+++ app/android/src/com/rocketteam/locator/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.rocketteam.locator;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SplashActivity extends Activity {
+	
+	private static long splashDelay = 6000;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		
+		TimerTask task = new TimerTask() {
+			
+			@Override
+			public void run() {
+				Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+		        startActivity(mainIntent);
+		        finish();
+			}
+		};
+		Log.w("Splash","Before timer");
+		Timer t = new Timer();
+		t.schedule(task, splashDelay);
+	}
+}
